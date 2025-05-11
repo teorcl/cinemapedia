c911a825f48e360faa040bf27c2d1c469d25cc44
Teodoro Calle Lara <teo.calle.lara@gmail.com>
2025-05-11 13:54:07 -0500

    feat(presentation): Se crea la pantalla de detalle de las peliculas

---
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class MovieScreen extends StatelessWidget {
+
+  static const String routeName = 'movie-screen';
+  final String movieId;
+
+  const MovieScreen({
+    super.key, 
+    required this.movieId,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Movie ID: $movieId'),
+      ),
+    );
+  }
+}
