a83f8380f44efeb116820b88ec5c84fc548e4dfa
Teodoro Calle Lara <teo.calle.lara@gmail.com>
2025-05-15 09:28:44 -0500

    feat(movie_screen): Se agrega el widget que muestra el listado de los actores

--- lib/presentation/providers/actors/actors_by_movie_provider.dart
+++ lib/presentation/providers/actors/actors_by_movie_provider.dart
@@ -4,25 +4,26 @@ import '../../../domain/entities/actor.dart';
 import '../providers.dart';
 
 final actorsByMovieProvider =
-    StateNotifierProvider<ActorsNotifier, List<Actor>>((ref) {
+    StateNotifierProvider<ActorsNotifier, Map<String, List<Actor>>>((ref) {
   final fetchActors = ref.watch(actorsRepositoryProvider).getActorsByMovie;
   return ActorsNotifier(fetchActors: fetchActors);
 });
 
 typedef ActorCallback = Future<List<Actor>> Function(String movieId);
 
-class ActorsNotifier extends StateNotifier<List<Actor>> {
+class ActorsNotifier extends StateNotifier<Map<String, List<Actor>>> {
   ActorCallback fetchActors;
 
-  ActorsNotifier({required this.fetchActors}) : super([]);
+  ActorsNotifier({required this.fetchActors}) : super({});
 
   Future<void> loadActors(String movieId) async {
+    if(state[movieId] != null) return; // Si ya tengo los actores, no vuelvo a hacer la peticion
 
     // state <----- List<Actor>
 
     final List<Actor> actors = await fetchActors(movieId);
 
     // state = <----- En este punto es una lista vacia, por la inicializacion en el constructor
-    state = [...state, ...actors]; // Estoy actualizando el estado
+    state = {...state, movieId: actors}; // Estoy actualizando el estado
   }
 }
--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -85,10 +86,10 @@ class _CustomSliverAppBar extends StatelessWidget {
               child: Image.network(
                 movie.posterPath,
                 fit: BoxFit.cover,
-                // loadingBuilder: (context, child, loadingProgress) {
-                //   if ( loadingProgress != null ) return const SizedBox();
-                //   return FadeIn(child: child);
-                // },
+                loadingBuilder: (context, child, loadingProgress) {
+                  if ( loadingProgress != null ) return const SizedBox();
+                  return FadeIn(child: child);
+                },
               ),
             ),
 
@@ -122,9 +123,6 @@ class _CustomSliverAppBar extends StatelessWidget {
                 ),
               ),
             ),
-
-
-
           ],
         ),
       ),
@@ -199,10 +197,81 @@ class _MovieDetails extends StatelessWidget {
           ),
         ),
 
-        // _ActorsByMovie(movieId: movie.id.toString() ),
+        const SizedBox( height: 8 ),
+        
+        Padding(
+          padding: const EdgeInsets.symmetric( horizontal: 10 ),
+          child: Text( 
+            'Actores', 
+            style: textStyles.titleLarge ,
+          ),
+        ),
+
+        _ActorsByMovie(movieId: movie.id.toString() ),
 
         const SizedBox(height: 50 ),
       ],
+    );
+  }
+}
+
+class _ActorsByMovie extends ConsumerWidget {
+
+  final String movieId;
+
+  const _ActorsByMovie({required this.movieId});
+
+  @override
+  Widget build(BuildContext context, ref) {
+
+    final actorsByMovie = ref.watch( actorsByMovieProvider );
+
+    if ( actorsByMovie[movieId] == null ) {
+      return const CircularProgressIndicator(strokeWidth: 2);
+    }
+    final actors = actorsByMovie[movieId]!;
+
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: actors.length,
+        itemBuilder: (context, index) {
+          final actor = actors[index];
+
+          return Container(
+            padding: const EdgeInsets.all(8.0),
+            width: 135,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+
+                // Actor Photo
+                FadeInRight(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: Image.network(
+                      actor.profilePath,
+                      height: 180,
+                      width: 135,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+
+                // Nombre
+                const SizedBox(height: 5,),
+
+                Text(actor.name, maxLines: 2 ),
+                Text(actor.character ?? '', 
+                  maxLines: 2,
+                  style: const TextStyle( fontWeight: FontWeight.bold, overflow: TextOverflow.ellipsis ),
+                ),
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
