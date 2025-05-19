ebd275638ffd8ff496fbbcc1cb104e99b6cfa5b9
Teodoro Calle Lara <teo.calle.lara@gmail.com>
2025-05-19 11:05:09 -0500

    fix(lib/infrastructure/models/moviedb/movie_details.dart): Se ajusta para el caso donde el imdbId llega null

--- lib/infrastructure/models/moviedb/movie_details.dart
+++ lib/infrastructure/models/moviedb/movie_details.dart
@@ -34,7 +34,7 @@ class MovieDetails {
     final List<Genre> genres;
     final String homepage;
     final int id;
-    final String imdbId;
+    final String? imdbId;
     final String originalLanguage;
     final String originalTitle;
     final String overview;
@@ -61,7 +61,7 @@ class MovieDetails {
         genres: List<Genre>.from(json['genres'].map((x) => Genre.fromJson(x))),
         homepage: json['homepage'],
         id: json['id'],
-        imdbId: json['imdb_id'],
+        imdbId: json['imdb_id'] ,
         originalLanguage: json['original_language'],
         originalTitle: json['original_title'],
         overview: json['overview'],
