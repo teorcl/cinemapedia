60391d9c82ddf19107f634b4f0d10e9c39475394
Teodoro Calle Lara <teo.calle.lara@gmail.com>
2025-05-20 12:51:08 -0500

    Se ajusta el modelo MovieMovieDB

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -27,7 +27,7 @@ class MovieMapper {
         (moviedb.posterPath != '')
             ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
             : 'no-poster',
-    releaseDate: moviedb.releaseDate,
+    releaseDate: moviedb.releaseDate != null ? moviedb.releaseDate! : DateTime.now(),
     title: moviedb.title,
     video: moviedb.video,
     voteAverage: moviedb.voteAverage,
