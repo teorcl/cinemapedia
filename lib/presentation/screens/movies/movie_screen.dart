ae89bbf3ee5dee2b8d890f6a6693e29223b4b838
Teodoro Calle Lara <teo.calle.lara@gmail.com>
2025-05-31 22:35:35 -0500

    fix(movie_screen.dart): Se ajunta screen para mostrar el icono de favoritos

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -56,13 +56,13 @@ class _MovieScreenState extends ConsumerState<MovieScreen> {
   }
 }
 
-class _CustomSliverAppBar extends StatelessWidget {
+class _CustomSliverAppBar extends ConsumerWidget {
   final Movie movie;
 
   const _CustomSliverAppBar({required this.movie});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final size = MediaQuery.of(context).size;
 
     return SliverAppBar(
@@ -73,7 +73,9 @@ class _CustomSliverAppBar extends StatelessWidget {
         IconButton(
           //icon: const Icon(Icons.favorite_border),
           icon: const Icon(Icons.favorite_rounded, color: Colors.red),
-          onPressed: () {},
+          onPressed: () {
+            ref.watch(localStorageRepositoryProvider).toggleFavorite(movie);
+          },
         ),
       ],
       flexibleSpace: FlexibleSpaceBar(
